4bbecf861a96b9880b2bb2426f5efd4c3a143f9a
JOBFER <gbringashotmail@hotmail.com>
2018-06-22 11:22:10 +0200

    Inicio

---
+++ src/java/com/videotutoriales/jaxrs/Alumno.java
@@ -0,0 +1,123 @@
+package com.videotutoriales.jaxrs;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "ALUMNO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Alumno.findAll", query = "SELECT a FROM Alumno a")
+    , @NamedQuery(name = "Alumno.findById", query = "SELECT a FROM Alumno a WHERE a.id = :id")
+    , @NamedQuery(name = "Alumno.findByNombre", query = "SELECT a FROM Alumno a WHERE a.nombre = :nombre")
+    , @NamedQuery(name = "Alumno.findByPrimerapellido", query = "SELECT a FROM Alumno a WHERE a.primerapellido = :primerapellido")
+    , @NamedQuery(name = "Alumno.findBySegundoapellido", query = "SELECT a FROM Alumno a WHERE a.segundoapellido = :segundoapellido")
+    , @NamedQuery(name = "Alumno.findByEmail", query = "SELECT a FROM Alumno a WHERE a.email = :email")})
+public class Alumno implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Integer id;
+    
+    @Size(max = 50)
+    @Column(name = "NOMBRE")
+    private String nombre;
+    
+    @Size(max = 50)
+    @Column(name = "PRIMERAPELLIDO")
+    private String primerapellido;
+    
+    @Size(max = 50)
+    @Column(name = "SEGUNDOAPELLIDO")
+    private String segundoapellido;
+    
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Size(max = 50)
+    @Column(name = "EMAIL")
+    private String email;
+
+    public Alumno() {
+    }
+
+    public Alumno(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerapellido() {
+        return primerapellido;
+    }
+
+    public void setPrimerapellido(String primerapellido) {
+        this.primerapellido = primerapellido;
+    }
+
+    public String getSegundoapellido() {
+        return segundoapellido;
+    }
+
+    public void setSegundoapellido(String segundoapellido) {
+        this.segundoapellido = segundoapellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Alumno)) {
+            return false;
+        }
+        Alumno other = (Alumno) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.videotutoriales.jaxrs.Alumno[ id=" + id + " ]";
+    }
+    
+}
